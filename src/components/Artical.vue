311cd3b51c953ea33483532701949d3fc0445d70
朱建山 <zhujianshan@zhujianshandeMacBook-Pro.local>
2017-09-25 16:27:14 +0800

    test

---
+++ src/components/Artical.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="f-artical">
+    <p class="f-menu">
+      <Icon type="android-menu"></Icon>
+      <span>
+        <Switch size="large" v-model="hide" @on-change="hideArtical">
+          <span slot="open">显示1</span>
+          <span slot="close">隐藏</span>
+        </Switch>
+      </span>   
+      <span class="f-close" @click="close">
+        <Icon type="close-round" ></Icon>
+      </span>
+    </p>
+    <p class="f-artical-title" v-text="editedArtical.title"></p>
+    <p class="f-artical-author" v-text="editedArtical.author"></p>
+    <p class="f-artical-content" v-text="editedArtical.content"></p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'f-artical',
+  data () {
+    return {
+      hide: true
+    }
+  },
+  props: ['artical'],
+  computed: {
+    editedArtical: function () {
+      if (this.artical) {
+        return this.artical
+      } else {
+        return {'title': '', 'author': '', 'content': ''}
+      }
+    }
+  },
+  mounted: function () {
+    console.log(this.artical)
+  },
+  watch: {
+  },
+  methods: {
+    close: function () {
+      this.$emit('close')
+    },
+    hideArtical: function (status) {
+      console.log(statusbar)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" scoped="" type="text/css">
+  .f-menu {
+    font-size: 30px;
+    color: gray;
+    .f-close {
+      float: right;
+    }
+    .f-close:hover {
+      color: #ed3f14;
+    }
+  }
+  .f-artical-title {
+    font-size: 40px;
+    text-align: center;
+  }
+  .f-artical-author {
+    font-size: 30px;
+    text-align: center;
+    color: gray;
+    padding-left: 20%;
+  }
+  .f-artical-content {
+    font-size: 20px;
+    text-align: center;
+  }
+</style>
--- src/components/Articals.vue
+++ src/components/Articals.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="f-articals">
-    <Row type="flex">
-      <template v-for="artical in articals">
+    <Row type="flex" v-show="!showDetail">
+      <template v-for="artical(artical, key) in articals">
         <Col span="7" order="4" class="f-artical">
           <Card :bordered="false" @click="showArtical(artical)">
+            <span class="f-close" @click="deleteArtical(key)"><Icon type="trash-a"></Icon></span>
             <p slot="title" v-text="artical.title" @click="showArtical(artical)"></p>
             <p slot="title" class="f-artical-author" v-text="artical.author" @click="showArtical(artical)"></p>
             <p class="f-artical-thumbnail" v-text="artical.content" @click="showArtical(artical)"></p>
@@ -11,61 +12,101 @@
         </Col>
       </template>
       <Col span="7" order="4" class="f-artical">
-          <Card :bordered="false" @click="showArtical(artical)">
-            <p class="f-artical-add"><Icon type="plus-round"></Icon></p>
-          </Card>
-        </Col>
+        <Card :bordered="false" @click="showArtical(artical)">
+          <p class="f-artical-add" @click="addArtical"><Icon type="plus-round"></Icon></p>
+        </Card>
+      </Col>
+      
+      <Modal v-model="modal1" title="新建文章" @on-ok="ok" @on-cancel="cancel" width="40" :styles="{top: '60px'}">
+        <Row type="flex">
+          <Col span="7" order="4" class="f-artical">
+            <p class="f-title">标题：</p>
+          </Col>
+          <Col span="17" order="4" class="f-artical">
+             <Input v-model="editArtical.title" placeholder="请输入文章标题..." style="width: 300px"></Input>
+          </Col>
+          <Col span="7" order="4" class="f-artical">
+            <p class="f-title">作者：</p>
+          </Col>
+          <Col span="17" order="4" class="f-artical">
+             <Input v-model="editArtical.author" placeholder="请输入文章作者..." style="width: 300px"></Input>
+          </Col>
+          <Col span="7" order="4" class="f-artical">
+            <p class="f-title">内容：</p>
+          </Col>
+          <Col span="17" order="4" class="f-artical">
+             <Input v-model="editArtical.content" type="textarea" placeholder="请输入文章内容..." :rows="4" style="width: 300px"></Input>
+          </Col>
+        </Row>
+      </Modal>
     </Row>
+    <artical v-show="showDetail" :artical="selectArtical" v-on:close="closeDetail"></artical>
   </div>
 </template>
 <script>
+import Artical from '@/components/Artical'
 export default {
   name: 'f-articals',
   data () {
     return {
-      articals: [
-        {
-          title: '望庐山瀑布',
-          author: '李白',
-          content: '日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天'
-        }, {
-          title: '早发白帝城',
-          author: '李白',
-          content: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。'
-        }, {
-          title: '早发白帝城',
-          author: '李白',
-          content: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。'
-        }, {
-          title: '早发白帝城',
-          author: '李白',
-          content: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。'
-        }, {
-          title: '早发白帝城',
-          author: '李白',
-          content: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。'
-        }, {
-          title: '早发白帝城',
-          author: '李白',
-          content: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。'
-        }, {
-          title: '早发白帝城',
-          author: '李白',
-          content: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。'
-        }
-      ]
+      articals: [],
+      modal1: false,
+      editArtical: {'title': '', 'author': '', 'content': ''},
+      selectArtical: null,
+      showDetail: false
     }
   },
   computed: {
   },
+  components: {
+    'artical': Artical
+  },
   mounted: function () {
-
+    if (localStorage.articals) {
+      this.articals = JSON.parse(localStorage.articals)
+      console.log(this.articals)
+    }
   },
   watch: {
   },
   methods: {
     showArtical: function (artical) {
       console.log(artical.title)
+      this.selectArtical = artical
+      this.showDetail = true
+    },
+    addArtical: function () {
+      console.log(this)
+      this.modal1 = true
+      // console.log(this.modal1)
+      // console.log(2123111)
+    },
+    ok: function () {
+      console.log(this.editArtical)
+      this.articals.push(this.editArtical)
+      this.editArtical = {'title': '', 'author': '', 'content': ''}
+      localStorage.articals = JSON.stringify(this.articals)
+        // this.$Message.info('点击了确定');
+    },
+    cancel: () => {
+        // this.$Message.info('点击了取消');
+    },
+    closeDetail: function () {
+      this.showDetail = false
+    },
+    deleteArtical: function (key) {
+      console.log(key)
+      this.$Modal.confirm({
+        // title: '确认对话框标题',
+        content: '确定要删除该文章？',
+        onOk: () => {
+          this.articals.splice(key, 1)
+          localStorage.articals = JSON.stringify(this.articals)
+        },
+        onCancel: () => {
+        }
+      })
+        // this.$Message.info('点击了取消');
     }
   }
 }
@@ -95,10 +136,45 @@ export default {
     font-size: 70px;
     text-align: center;
     height: 115px;
-    color: #2d8cf0;
+    color: gray;
     // vertical-align: middle;
     // padding-top: 200x;
   }
+  .f-artical-add:hover {
+    font-size: 70px;
+    text-align: center;
+    height: 115px;
+    color:#2d8cf0;
+    // vertical-align: middle;
+    // padding-top: 200x;
+  }
+  .ivu-modal{
+    top: 0;
+    height: 100vh;
+  }
+  .f-close {
+    position: absolute;
+    right: 10px;
+    bottom: 0px;
+    font-size: 20px;
+    color: gray;
+    -webkit-transition:opacity 0.5s;
+    opacity:0; 
+    pointer-events:none;
+  }
+  .f-artical:hover .f-close{
+    -webkit-transition:opacity 0.2s;
+    opacity:1; 
+    pointer-events:auto;
+  }
+  .f-close:hover {
+    color: red;
+  }
 
 }
+.f-title {
+  text-align: right;
+  padding-top: 6px;
+}
+
 </style>
